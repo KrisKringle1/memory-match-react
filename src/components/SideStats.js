2eb9bcce09ff80f0701596f608ee1d2c5daec542
Kris Ringler <kmringler@gmail.com>
2020-05-04 00:00:33 -0700

    this old code is a mess - working on adding in sidebar

---
+++ src/components/SideStats.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export const SideStats = () => {
+  return (
+    <main className="main">
+      <aside className="sideBar">
+
+
+        <div className="stats belwelight" id="stats2">Games<br><span className="gamesPlayed">0</span></div>
+
+
+          <div className="stats belwelight gamesPlayed" id="stats3">0</div>
+
+          <div className="stats belwelight" id="stats4">Attempts <br><span className="attemptsMade">0</span></div>
+
+
+            <div className="stats belwelight attemptsMade" id="stats5">0</div>
+
+            <div className="stats belwelight" id="stats6">Accuracy <br><span className="gameAccuracy">0%</span></div>
+
+
+              <div className="stats belwelight gameAccuracy" id="stats7">0%</div>
+
+		</aside>
+
+            <div className="container"></div>
+	</main>
+  )
+}
